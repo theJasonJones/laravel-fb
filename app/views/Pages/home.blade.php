c1ef92c2106718e9bdcdb0dda600bd24e8cd4bc6
Jason Jones <jkjmr6@gmail.com>
2014-08-24 20:11:04 +0000

    Added bootstrap styles

---
+++ app/views/Pages/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.default')
+@section('content')
+    <div class="jumbotron">
+        <h1>Welcome to Larabook!</h1>
+        <p>Welcome to the premier place to talk about Laravel with others. Why don't you sign up and see what all the fuss is about.
+        <p>
+            <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">Sign Up!</a>
+        </p>
+    </div>
+@stop
---
+++ app/views/layouts/default.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Larabook</title>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"/>
+</head>
+<body>
+    @include('layouts.partials.nav')
+    <div class="container">
+        @yield('content')
+    </div>
+
+
+<script src="//code.jquery.com/jquery-git2.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</body>
+</html>
