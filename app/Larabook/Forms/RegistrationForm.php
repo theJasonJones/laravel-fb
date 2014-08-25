9542771fef83bee1726fabbf28d9f4588aa308e3
Jason Jones <jkjmr6@gmail.com>
2014-08-25 04:02:54 +0000

    Added Validation rules

---
+++ app/Larabook/Forms/RegistrationForm.php
@@ -0,0 +1,14 @@
+<?php namespace Larabook\Forms;
+use Laracasts\Validation\FormValidator;
+
+class RegistrationForm extends FormValidator{
+
+    /*
+     * Validation rules for the registration form
+     */
+    protected $rules = [
+        'username' => 'required|unique:users',
+        'email'     => 'required|email|unique:users',
+        'password'  => 'required|confirmed'
+    ];
+}
