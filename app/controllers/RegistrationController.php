d9c93abfc76a4bff62be0c8d27f2e3c00934450a
Jason Jones <jkjmr6@gmail.com>
2014-08-26 15:44:50 +0000

    Check for input, validate and register

--- app/controllers/RegistrationController.php
+++ app/controllers/RegistrationController.php
@@ -1,8 +1,18 @@
 <?php
 
+use Larabook\Forms\RegistrationForm;
+use Larabook\Registration\RegisterUserCommand;
+use Larabook\Core\CommandBus;
 class RegistrationController extends \BaseController {
 
+    use CommandBus;
 
+    private $registrationForm;
+
+    function __construct( RegistrationForm $registrationForm)
+    {
+        $this->registrationForm = $registrationForm;
+    }
 	/**
 	 * Show the form to register a user.
 	 *
@@ -20,8 +30,12 @@ class RegistrationController extends \BaseController {
      */
     public function store()
     {
-        $user = User::create(
-           Input::only('username', 'email', 'password')
+        extract(Input::only('username', 'email', 'password'));
+        $this->registrationForm->validate(Input::all());
+        $command = new RegisterUserCommand($username, $email, $password);
+
+        $user = $this->execute(
+            new RegisterUserCommand($username, $email, $password)
         );
 
         Auth::login($user);
